78f104639a5eb40e4fa1fa9b1e2d16ef8d354287
abdur-rafi <abdurrafi403@gmail.com>
2022-02-22 15:48:06 +0600

    show info of current role in sidebar: Components created

--- components/contents/options/UserOptions.tsx
+++ components/contents/options/UserOptions.tsx
@@ -4,6 +4,7 @@ import React, {Fragment, useContext, useEffect, useState} from "react";
 import {useRouter} from "next/router";
 import styles from './UserOptions.module.scss';
 import AuthContext from "../../../store/auth-context";
+import { CSSProperties } from "@emotion/serialize";
 
 const UserOptions: React.FC<{ id: string }> = (props) => {
 
@@ -63,8 +64,19 @@ const UserOptions: React.FC<{ id: string }> = (props) => {
             <Offcanvas className={`${styles.background} text-light`} show={showSideBar}
                        onHide={handleCloseSideBar} scroll={true}>
                 <div className='text-lg-center'>
-                    <Offcanvas.Header closeButton closeVariant='white'>
-                        <Offcanvas.Title>{mode}&nbsp;{props.id}</Offcanvas.Title>
+                    <Offcanvas.Header closeButton closeVariant='white' style={{alignItems : "baseline"}}>
+                        {/* <Offcanvas.Title>{mode}&nbsp;{props.id}</Offcanvas.Title> */}
+                        
+                        <Offcanvas.Title>
+                            {/* <TeacherInfo DEPARTMENT_NAME="Mechanical Engineering" ROLE_ID={12}
+                            UNIVERSITY_NAME = "Bangladesh Univeristy of Engineering and technology"
+                            /> */}
+                            <StudentInfo BATCH_YEAR={2016} DEPARTMENT_NAME = "Computer Science and Engineering"
+                            ROLE_ID={12} SECTION_NAME = {"E"} SECTION_ROLL_NO = {5} UNIVERSITY_NAME = "Bangladesh University Of Engineering And Technology"
+                            />
+                            {/* <ManagementInfo MANAGEMENT_ID={12} UNIVERSITY_NAME = "Bangladesh University Of Engineering And Technology" /> */}
+                        </Offcanvas.Title>
+
                     </Offcanvas.Header>
                 </div>
                 <Offcanvas.Body>
@@ -92,6 +104,73 @@ const UserOptions: React.FC<{ id: string }> = (props) => {
             </Offcanvas>
         </Fragment>
     );
+}
+
+
+const containerStyle  : React.CSSProperties = {
+    display : "flex",
+    flexDirection  : "column",
+    textAlign : "left",
+    padding : "10px", 
+    border : "solid 1px white"
+}
+const itemStyle  : React.CSSProperties = {
+    fontSize : "14px",
+    padding : "2px"
+}
+
+
+const TeacherInfo : React.FC<{
+    ROLE_ID : string | number,
+    DEPARTMENT_NAME : string,
+    UNIVERSITY_NAME : string
+}> = (props)=>{
+    return(
+        <div style={containerStyle}>
+            <div > Teacher </div>
+            <div style={itemStyle}> Id : {props.ROLE_ID}</div>
+
+            <div style={itemStyle}> {props.DEPARTMENT_NAME} &nbsp;Department </div>
+            <div style={itemStyle} >{props.UNIVERSITY_NAME}</div>
+        </div>
+    )
+}
+
+
+
+const StudentInfo : React.FC<{
+    ROLE_ID: number | string,
+    BATCH_YEAR: number,
+    DEPARTMENT_NAME: string,
+    SECTION_NAME: string,
+    SECTION_ROLL_NO: number | string,
+    UNIVERSITY_NAME : string
+}> = (props)=>{
+    return (
+        <div  style = {containerStyle}>
+            <div> Student </div>
+            <div style={itemStyle}> Id : {props.ROLE_ID}</div>
+
+            <div style = {itemStyle}> {props.DEPARTMENT_NAME} - {props.BATCH_YEAR} Batch </div>
+            <div style = {itemStyle}> {props.SECTION_NAME} section. Roll No : {props.SECTION_ROLL_NO} </div>
+            <div style = {itemStyle}> {props.UNIVERSITY_NAME}</div>
+
+        </div>
+    )
+}
+
+const ManagementInfo : React.FC<{
+    UNIVERSITY_NAME : string,
+    MANAGEMENT_ID : number | string
+}> = (props)=>{
+    return (
+        
+        <div style={containerStyle}>
+            <div > Management </div>
+            <div style={itemStyle}> Id : {props.MANAGEMENT_ID}</div>
+            <div style={itemStyle} >{props.UNIVERSITY_NAME}</div>
+        </div>
+    )
 }
 
 export default UserOptions;
